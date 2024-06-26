ceedb95d4acbdef141d1fb3cdd002e85ac7941c5
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2024-06-26 23:35:53 +0530

    prog1.c

---
+++ Assignment- 4/prog1.c
@@ -0,0 +1,19 @@
+/*Write a C program to print the following asterisk graph:
+*
+* *
+* * *
+* * * * (Up to n numbers of row)*/
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+    for(i = 1 ; i <= n ; i++){
+        for(j = 1 ; j <= i ; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
