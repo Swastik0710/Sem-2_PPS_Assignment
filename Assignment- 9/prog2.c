7471f82e447f1c3b90fc4690674a5bd05f39b944
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2024-07-04 20:51:19 +0530

    prog2.c

---
+++ Assignment- 9/prog2.c
@@ -0,0 +1,18 @@
+//Write a C Program to swap two given numbers using call by reference.
+#include<stdio.h>
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main() {
+    int num1, num2;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
+    swap(&num1, &num2);
+    printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
+    return 0;
+}
