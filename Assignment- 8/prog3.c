4db6c9f938d1e0f84d6fcce96134a558828ea6d7
Swastik Bhattacharya <152956696+Swastik0710@users.noreply.github.com>
2024-07-04 20:40:50 +0530

    prog3.c

---
+++ Assignment- 8/prog3.c
@@ -0,0 +1,39 @@
+//Write a C Program to find the sum of two matrices using function.
+#include<stdio.h>
+void addMatrices(int rows, int cols, int mat1[rows][cols], int mat2[rows][cols], int result[rows][cols]) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+}
+int main() {
+    int rows, cols;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+    int mat1[rows][cols], mat2[rows][cols], result[rows][cols];
+    printf("Enter elements of the first matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+    printf("Enter elements of the second matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+    addMatrices(rows, cols, mat1, mat2, result);
+    printf("The sum of the two matrices is:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
